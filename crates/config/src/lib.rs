12b199e43dd7a006571d32e807a51352011effcd
epociask <empociask@dons.usfca.edu>
2023-02-05 03:46:52 -0800

    Updated Config

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -1,15 +1,69 @@
 use envconfig::Envconfig;
+use core::str::Split;
 
+// Single config struct to store all env variables
 #[derive(Clone, Envconfig)]
 pub struct Config {
     #[envconfig(from = "API_HOST")]
-    pub api_host: String,
+    api_host: String,
     #[envconfig(from = "API_PORT")]
-    pub api_port: u16,
+    api_port: u16,
     #[envconfig(from = "MAX_MEMPOOL")]
-    pub max_mempool: usize,
+    max_mempool: usize,
     #[envconfig(from = "ECDSA_PUB_KEY")]
-    pub pub_key: String,
+    pub_key: String,
     #[envconfig(from = "ECDSA_PRIV_KEY")]
-    pub priv_key: String,
+    priv_key: String,
+    #[envconfig(from = "PEER_IDS")]
+    peers_str: String
+}
+
+impl Config {
+    pub fn api_host(&self) -> String {
+        self.api_host.clone()
+    }
+
+    pub fn api_port(&self) -> u16 {
+        self.api_port.clone()
+    }
+
+    pub fn max_mempool(&self) -> usize {
+        self.max_mempool
+    }
+
+    pub fn public_key(&self) -> String {
+        self.pub_key.clone()
+    }
+
+    pub fn private_key(&self) -> String {
+        self.priv_key.clone()
+    }
+
+    pub fn peer_ids(&self) -> Vec<String> {
+        let ids: Split<&str> = self.peers_str.split(",");
+        let mut static_vec: Vec<String> = vec![];
+
+        ids.for_each(|val: &str| {
+            static_vec.push(val.into());
+        });
+
+        static_vec
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_peer_ids() {
+        let cfg = Config{
+            api_host: String::from(""), api_port: 0, 
+            max_mempool: 0, pub_key: String::from(""),
+            priv_key: String::from(""),
+            peers_str: String::from("a,b,c"),
+        };
+
+        assert_eq!(cfg.peer_ids(), vec!["a", "b", "c"]);
+    }
 }
--- crates/p2p/README.md
+++ crates/p2p/README.md
@@ -1,15 +1,14 @@
 # Sloppy Chain P2P
 
-## Features
-
-### Peer Discovery
+## Key Flows
 Node should be able to discover peers given a set of existing peers. Node will:
-1. Ask existing peers for their peers using a _Depth First Search (DFS)_ until `PEER_LIMIT` is maxed out or no other peers can be found.
-2. Maintain a `peer_list` where inactive peers are removed and cycled
-3. 
-
+1. [ ] Ask existing peers for their peers using a _Depth First Search (DFS)_ until `PEER_LIMIT` is maxed out or no other peers can be found.
+2. [ ] Maintain a `peer_list` where inactive peers are removed and cycled
+3. [ ] Broadcast both transactions and blocks to other peers
+4. [ ] Be able to bootstrap state through downloading blocks from another node
+5. [ ] Listen for transactions and blocks from peers and update mempool / state accordingly
+6. [ ] Listen for peer pairing requests and pair accordingly
 
 
 ## Resource(s)
 - [Unresponsive BTC Nodes](http://www.petecorey.com/blog/2018/07/09/ping-pong-and-unresponsive-bitcoin-nodes/)
-- 
